a48de60d9397d2710fb3cafa774d8bf31574f55b
eren-the-coder <jeanpc568@gmail.com>
2025-11-18 10:51:58 +0100

    feat: add HomeHeader component

---
+++ src/ui/components/headers/HomeHeader/HomeHeader.tsx
@@ -0,0 +1,21 @@
+import styles from "./HomeHeader.module.css";
+import { useTheme } from "../../../hooks/useTheme";
+import { FiSun, FiMoon } from "react-icons/fi";
+
+export const HomeHeader = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>MyStreaks</h1>
+
+      <button
+        className={styles.themeButton}
+        aria-label="Changer de thème"
+        onClick={toggleTheme}
+      >
+        {theme === "light" ? <FiMoon /> : <FiSun />}
+      </button>
+    </header>
+  );
+};
