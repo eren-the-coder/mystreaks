694e1bfc65fad819bcdd7058eb834d610b97148e
eren-the-coder <jeanpc568@gmail.com>
2025-11-20 01:17:27 +0100

    feat: implement app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Home } from './ui/pages/Home/Home.tsx';
 import { RoutineProvider } from './context/RoutineContext.tsx'
+import { Details } from './ui/pages/Details/Details.tsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RootLayout } from './ui/pages/RootLayout/RootLayout.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "details/:routineId", element: <Details /> },
+    ],
+  },
+  {
+    path: "*",
+    element: <>
+      {/* <Header />
+      <div className="page">
+        <NotFound />
+      </div>
+      <Footer /> */}
+    </>,
+  },
+]);
 
 const App: React.FC = () => {
 
   return (
     <RoutineProvider>
-      <Home />      
+      <RouterProvider router={router} />
     </RoutineProvider>
   );
 };
